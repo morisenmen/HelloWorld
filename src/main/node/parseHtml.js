d9882779916ac83ce759a836cc1a0340de8300c2
morisenmen <morisenmen@gmail.com>
2014-11-20 21:38:18 +0800

    parse html use node

---
+++ src/main/node/parseHtml.js
@@ -0,0 +1,61 @@
+﻿var http = require("http");
+var fs = require("fs");
+var parser = require("./parsePrice");
+
+var url = "http://car.autohome.com.cn/";
+var iconv = require("iconv-lite");
+var data;
+var regExp = /<span class="open-name"><a href="(.+?)">(.+?)<\/a><\/span>/gim
+var dest="car";
+var getUrl = function(baseUrl, url) {
+	return url.indexOf(baseUrl) >= 0 ? url : baseUrl + url;
+}
+var mkdir = function(dir, callback) {
+	if (!fs.existsSync(dir)) {
+		fs.mkdir(dir, callback);
+	} else {
+		callback();
+	}
+}
+var req = http.request(url, function(res){
+	res.on('data', function(chunk){
+		data += iconv.decode(chunk, "gb2312");
+	}).on('end', function(){
+		mkdir(dest, function(err) {
+			if (err) throw err;
+			var result = "";
+			while(rs = regExp.exec(data)) {
+				var destUrl = getUrl(url, rs[1]);
+				var dir = dest+"/"+rs[2];
+				result += rs[2] + "::" + destUrl + "\n";
+				mkdir(dir, function(err) {
+					if (err) throw err;
+					parser.saveFile(destUrl, dir);
+					// var rq = http.request(destUrl, function(resp) {
+					// 	var dt = "";
+					// 	resp.on("data", function(ck) {
+					// 		dt += iconv.decode(ck, "gb2312");
+					// 	}).on("end", function(){
+					// 		fs.writeFile(dir + "/origin.html", dt, function (err) {
+					// 			if (err) throw err;
+					// 			console.log(rs[2] + "Origin saved!");
+					// 		});
+					// 	});
+					// });
+
+					// rq.end();
+				});
+			}
+			fs.writeFile(dest + "/dest.html", result, function(err) {
+				if (err) throw err;
+	    		console.log('It\'s saved!');
+			});
+		});
+		fs.writeFile(dest + "/origin.html", data, function (err) {
+			if (err) throw err;
+			console.log("Origin saved!");
+		});
+	});
+});
+
+req.end();
---
+++ src/main/node/parsePrice.js
@@ -0,0 +1,35 @@
+﻿var http = require("http");
+var fs = require("fs");
+var iconv = require("iconv-lite");
+
+var regExp = /<div class="list-cont-main"><div.*?><a.*?>(.*?)<\/a><\/div>.*?<div class="main-lever-right"><div.*?<span.*?<span.*?>(.+?)<\/span><\/span><\/div>.*?(<ul class="interval01-list">.*?<\/ul>).+?<\/div><\/div>/gim
+var exp = /<p class="infor-title"><a.*?>(.*?)<\/a><\/p>.*?<span class="guidance-price">(.*?)<\/span>/gim
+
+exports.saveFile = function(url, destDir) {
+	var data = "";
+	var req = http.request(url, function(res){
+		res.on('data', function(chunk){
+			data += iconv.decode(chunk, "gb2312");
+		}).on('end', function(){
+			var result = "";
+			while(rs = regExp.exec(data)) {
+				result += rs[1] + ":" + rs[2] + "\n";
+				while(ps = exp.exec(rs[3])) {
+					result += "\t" + ps[1] + ":" + ps[2] + "\n";
+				}
+			}
+			var origin = destDir + "/dest.html";
+			var dest = destDir + "/origin.html";
+			fs.writeFile(origin, result, function(err) {
+				if (err) throw err;
+	    		console.log(origin + ' saved!');
+			});
+			fs.writeFile(dest, data, function (err) {
+				if (err) throw err;
+				console.log(dest + ' saved!');
+			});
+		});
+	});
+
+	req.end();
+}
